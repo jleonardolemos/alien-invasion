5b120137732269259a9d04b6dc08ca1aa140b3eb
Leonardo Lemos <leonardo.lemos@convenia.com.br>
2022-01-03 10:46:22 -0300

    build the alien fleet

--- alien_invasion.py
+++ alien_invasion.py
@@ -11,14 +11,14 @@ class AlienInvasion:
         pygame.init()
         self.settings = Settings()
 
-        # self.screen = pygame.display.set_mode((
-        #     self.settings.screen_width,
-        #     self.settings.screen_height
-        # ))
+        self.screen = pygame.display.set_mode((
+            self.settings.screen_width,
+            self.settings.screen_height
+        ))
 
-        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-        self.settings.screen_width = self.screen.get_rect().width
-        self.settings.screen_height = self.screen.get_rect().height
+        # self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        # self.settings.screen_width = self.screen.get_rect().width
+        # self.settings.screen_height = self.screen.get_rect().height
 
         pygame.display.set_caption('Alien Invaders')
 
@@ -87,6 +87,24 @@ class AlienInvasion:
 
     def _create_fleet(self):
         alien = Alien(self)
+        alien_width, alien_height = alien.rect.size
+        available_space_x = self.settings.screen_width - (2 * alien_width)
+        number_aliens_x = available_space_x // (2 * alien_width)
+        
+        ship_height = self.ship.rect.height
+        available_space_y = (self.settings.screen_height - (3 * alien_height) - ship_height)
+        number_rows = available_space_y // (2 * alien_height)
+
+        for row_number in range(number_rows):
+            for alien_number in range(number_aliens_x):
+                self._create_alien(alien_number, row_number)
+
+    def _create_alien(self, alien_number, row_number):
+        alien = Alien(self)
+        alien_width, alien_height = alien.rect.size
+        alien.x = alien_width + 2 * alien_width * alien_number
+        alien.rect.x = alien.x
+        alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
         self.aliens.add(alien)
 
 if __name__ == '__main__':
