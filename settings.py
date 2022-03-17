6beda4a3a28e98dabdd3a102831f057d62b7be4e
Leonardo Lemos <leonardo.lemos@convenia.com.br>
2022-03-16 23:55:09 -0300

    load settings from HD

--- settings.py
+++ settings.py
@@ -1,4 +1,5 @@
 from os import path, makedirs
+import json
 
 class Settings:
     """A class to store all settings for Alien Invasion."""
@@ -44,6 +45,33 @@ class Settings:
         self.alien_speed *= self.speedup_scale
         self.alien_points = int(self.alien_points * self.score_scale)
     
-    def __load_config(self, settings_dir = "~"):
+    def __load_config(self, settings_dir = "~", config_file = "alien_settings.json"):
         dir = path.expanduser(self.settings_dir) + "/l30/alien"
         makedirs(dir, exist_ok=True)
+
+        try:
+            file = open(dir + "/" + config_file)
+            settings = json.load(file)
+        except FileNotFoundError:
+            print("Config file not found at " + dir + "/" + config_file + ".")
+            print("Creating config file.")
+        except json.decoder.JSONDecodeError:
+            print("Config file format error.")
+            print("Override config file.")
+
+    def __load_default_values(self):
+        return {
+            "player_name": "",
+            "screen_width": 1200,
+            "screen_height": 800,
+            "bg_color": (230, 230, 230),
+            "bg_image": 'images/background.bmp',
+            "ship_speed": 1.5,
+            "ship_limit": 3,
+            "alien_speed": 1.0,
+            "fleet_direction": 1,
+            "fleet_drop_speed": 10,
+            "speedup_scale": 1.1,
+            "score_scale": 1.2,
+            "alien_points": 50,
+        }
